a3475ad364fb723c1a86a33ecb81fa86909795bc
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-04-27 16:35:14 -0700

    Simple index.html struct and style refactoring.

--- src/templates/main.py
+++ src/templates/main.py
@@ -1,9 +1,17 @@
 from typing import List, Optional
 
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Field
+from starlette.requests import Request
 
-api = FastAPI()
+from src.classes.compound import Compound
+
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 # class Compound:
 
@@ -16,11 +24,10 @@ api = FastAPI()
 
 class CompoundBase(BaseModel):
     compound_name: str = Field(..., min_length=1, max_length=15, description="Name of the compound")
-    compound_x_cord: int = Field(..., min_length=1, description="X-cord of the compound")
-    compound_y_cord: int = Field(..., min_length=1, description="Y-cord of the compound")
-    compound_ret_fact: float = Field(..., min_length=2, description="Retention Factor of the compound")
+    compound_ret_fact: float = Field(..., gt=0, min_length=2, description="Retention Factor of the compound")
+
+@app.post("/simulate")
+async def simulate(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@api.get("/")
-def home():
-    return "<h1>Hello</h1>"
